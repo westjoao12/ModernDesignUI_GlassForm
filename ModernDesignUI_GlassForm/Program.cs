3c612234e5ef1daed40e0c1d88cc7f5fbe938fc6
westjoao12 <westjoao12@gmail.com>
2024-11-03 16:05:28 +0100

    Create and organize principal dashboard

--- ModernDesignUI_GlassForm/Program.cs
+++ ModernDesignUI_GlassForm/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ModernDesignUI_GlassForm.Forms_Principal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace ModernDesignUI_GlassForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DashboardPrincipal());
         }
     }
 }
