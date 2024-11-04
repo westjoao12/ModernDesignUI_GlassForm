e18c94ed720d761f95e8a2b9c542acdd7304cfa5
westjoao12 <westjoao12@gmail.com>
2024-11-04 02:09:03 +0100

    designing principal dashboard

--- ModernDesignUI_GlassForm/Forms Principal/DashboardPrincipal.Designer.cs
+++ ModernDesignUI_GlassForm/Forms Principal/DashboardPrincipal.Designer.cs
@@ -28,7 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DashboardPrincipal));
+            this.P_I = new System.Windows.Forms.Panel();
+            this.P_S = new System.Windows.Forms.Panel();
+            this.panelLateral = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.P_L = new System.Windows.Forms.Panel();
+            this.panelCabecalho = new Bunifu.Framework.UI.BunifuCards();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnResumoSystem = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.btnHome = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.lblTotalNotificacao = new System.Windows.Forms.Label();
+            this.btnNotificacao = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.panelUsuario = new System.Windows.Forms.Panel();
+            this.btnTrocarUsuario = new Bunifu.Framework.UI.BunifuImageButton();
+            this.lblNivelUsuario = new System.Windows.Forms.Label();
+            this.lblNomeUsuario = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.ptr_User = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.btnMenuLateral = new Bunifu.Framework.UI.BunifuImageButton();
+            this.panelCabecalho.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panelUsuario.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnTrocarUsuario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptr_User)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMenuLateral)).BeginInit();
             this.SuspendLayout();
+            // 
+            // P_I
+            // 
+            this.P_I.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.P_I.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.P_I.Location = new System.Drawing.Point(260, 640);
+            this.P_I.Name = "P_I";
+            this.P_I.Size = new System.Drawing.Size(944, 10);
+            this.P_I.TabIndex = 3;
+            // 
+            // P_S
+            // 
+            this.P_S.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.P_S.Dock = System.Windows.Forms.DockStyle.Top;
+            this.P_S.Location = new System.Drawing.Point(260, 0);
+            this.P_S.Name = "P_S";
+            this.P_S.Size = new System.Drawing.Size(944, 10);
+            this.P_S.TabIndex = 2;
+            // 
+            // panelLateral
+            // 
+            this.panelLateral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
+            this.panelLateral.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelLateral.BackgroundImage")));
+            this.panelLateral.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelLateral.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLateral.GradientBottomLeft = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
+            this.panelLateral.GradientBottomRight = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
+            this.panelLateral.GradientTopLeft = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
+            this.panelLateral.GradientTopRight = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(58)))), ((int)(((byte)(183)))));
+            this.panelLateral.Location = new System.Drawing.Point(0, 0);
+            this.panelLateral.Name = "panelLateral";
+            this.panelLateral.Quality = 10;
+            this.panelLateral.Size = new System.Drawing.Size(250, 650);
+            this.panelLateral.TabIndex = 0;
+            // 
+            // P_L
+            // 
+            this.P_L.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.P_L.Dock = System.Windows.Forms.DockStyle.Left;
+            this.P_L.Location = new System.Drawing.Point(250, 0);
+            this.P_L.Name = "P_L";
+            this.P_L.Size = new System.Drawing.Size(10, 650);
+            this.P_L.TabIndex = 1;
+            // 
+            // panelCabecalho
+            // 
+            this.panelCabecalho.BackColor = System.Drawing.Color.White;
+            this.panelCabecalho.BorderRadius = 0;
+            this.panelCabecalho.BottomSahddow = true;
+            this.panelCabecalho.color = System.Drawing.Color.White;
+            this.panelCabecalho.Controls.Add(this.panel2);
+            this.panelCabecalho.Controls.Add(this.btnMenuLateral);
+            this.panelCabecalho.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelCabecalho.LeftSahddow = false;
+            this.panelCabecalho.Location = new System.Drawing.Point(260, 10);
+            this.panelCabecalho.Name = "panelCabecalho";
+            this.panelCabecalho.RightSahddow = false;
+            this.panelCabecalho.ShadowDepth = 20;
+            this.panelCabecalho.Size = new System.Drawing.Size(944, 55);
+            this.panelCabecalho.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.panelUsuario);
+            this.panel2.Location = new System.Drawing.Point(588, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(354, 48);
+            this.panel2.TabIndex = 19;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnResumoSystem);
+            this.panel1.Controls.Add(this.btnHome);
+            this.panel1.Controls.Add(this.lblTotalNotificacao);
+            this.panel1.Controls.Add(this.btnNotificacao);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(32, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(122, 48);
+            this.panel1.TabIndex = 19;
+            // 
+            // btnResumoSystem
+            // 
+            this.btnResumoSystem.CheckedState.Parent = this.btnResumoSystem;
+            this.btnResumoSystem.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnResumoSystem.HoverState.Image = ((System.Drawing.Image)(resources.GetObject("btnResumoSystem.HoverState.Image")));
+            this.btnResumoSystem.HoverState.Parent = this.btnResumoSystem;
+            this.btnResumoSystem.Image = ((System.Drawing.Image)(resources.GetObject("btnResumoSystem.Image")));
+            this.btnResumoSystem.Location = new System.Drawing.Point(10, 15);
+            this.btnResumoSystem.Name = "btnResumoSystem";
+            this.btnResumoSystem.PressedState.Parent = this.btnResumoSystem;
+            this.btnResumoSystem.Size = new System.Drawing.Size(25, 25);
+            this.btnResumoSystem.TabIndex = 84;
+            // 
+            // btnHome
+            // 
+            this.btnHome.CheckedState.Parent = this.btnHome;
+            this.btnHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHome.HoverState.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.HoverState.Image")));
+            this.btnHome.HoverState.Parent = this.btnHome;
+            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
+            this.btnHome.Location = new System.Drawing.Point(44, 15);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.PressedState.Parent = this.btnHome;
+            this.btnHome.Size = new System.Drawing.Size(25, 25);
+            this.btnHome.TabIndex = 83;
+            // 
+            // lblTotalNotificacao
+            // 
+            this.lblTotalNotificacao.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTotalNotificacao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(23)))), ((int)(((byte)(64)))));
+            this.lblTotalNotificacao.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalNotificacao.ForeColor = System.Drawing.Color.White;
+            this.lblTotalNotificacao.Location = new System.Drawing.Point(95, 4);
+            this.lblTotalNotificacao.Name = "lblTotalNotificacao";
+            this.lblTotalNotificacao.Size = new System.Drawing.Size(25, 25);
+            this.lblTotalNotificacao.TabIndex = 22;
+            this.lblTotalNotificacao.Text = "0";
+            this.lblTotalNotificacao.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTotalNotificacao.Visible = false;
+            // 
+            // btnNotificacao
+            // 
+            this.btnNotificacao.CheckedState.Parent = this.btnNotificacao;
+            this.btnNotificacao.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNotificacao.HoverState.Parent = this.btnNotificacao;
+            this.btnNotificacao.Location = new System.Drawing.Point(79, 15);
+            this.btnNotificacao.Name = "btnNotificacao";
+            this.btnNotificacao.PressedState.Parent = this.btnNotificacao;
+            this.btnNotificacao.Size = new System.Drawing.Size(25, 25);
+            this.btnNotificacao.TabIndex = 11;
+            // 
+            // panelUsuario
+            // 
+            this.panelUsuario.Controls.Add(this.btnTrocarUsuario);
+            this.panelUsuario.Controls.Add(this.lblNivelUsuario);
+            this.panelUsuario.Controls.Add(this.lblNomeUsuario);
+            this.panelUsuario.Controls.Add(this.pictureBox6);
+            this.panelUsuario.Controls.Add(this.ptr_User);
+            this.panelUsuario.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelUsuario.Location = new System.Drawing.Point(154, 0);
+            this.panelUsuario.Name = "panelUsuario";
+            this.panelUsuario.Size = new System.Drawing.Size(200, 48);
+            this.panelUsuario.TabIndex = 18;
+            // 
+            // btnTrocarUsuario
+            // 
+            this.btnTrocarUsuario.BackColor = System.Drawing.Color.White;
+            this.btnTrocarUsuario.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTrocarUsuario.Image = ((System.Drawing.Image)(resources.GetObject("btnTrocarUsuario.Image")));
+            this.btnTrocarUsuario.ImageActive = null;
+            this.btnTrocarUsuario.Location = new System.Drawing.Point(178, 15);
+            this.btnTrocarUsuario.Name = "btnTrocarUsuario";
+            this.btnTrocarUsuario.Size = new System.Drawing.Size(16, 16);
+            this.btnTrocarUsuario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnTrocarUsuario.TabIndex = 9;
+            this.btnTrocarUsuario.TabStop = false;
+            this.btnTrocarUsuario.Zoom = 10;
+            // 
+            // lblNivelUsuario
+            // 
+            this.lblNivelUsuario.AutoSize = true;
+            this.lblNivelUsuario.BackColor = System.Drawing.Color.White;
+            this.lblNivelUsuario.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNivelUsuario.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblNivelUsuario.Location = new System.Drawing.Point(56, 25);
+            this.lblNivelUsuario.Name = "lblNivelUsuario";
+            this.lblNivelUsuario.Size = new System.Drawing.Size(39, 17);
+            this.lblNivelUsuario.TabIndex = 12;
+            this.lblNivelUsuario.Text = "Nivel";
+            // 
+            // lblNomeUsuario
+            // 
+            this.lblNomeUsuario.AutoSize = true;
+            this.lblNomeUsuario.BackColor = System.Drawing.Color.White;
+            this.lblNomeUsuario.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeUsuario.Location = new System.Drawing.Point(56, 8);
+            this.lblNomeUsuario.Name = "lblNomeUsuario";
+            this.lblNomeUsuario.Size = new System.Drawing.Size(89, 16);
+            this.lblNomeUsuario.TabIndex = 11;
+            this.lblNomeUsuario.Text = "Nome usuario";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(37, 35);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 10;
+            this.pictureBox6.TabStop = false;
+            // 
+            // ptr_User
+            // 
+            this.ptr_User.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ptr_User.Image = ((System.Drawing.Image)(resources.GetObject("ptr_User.Image")));
+            this.ptr_User.Location = new System.Drawing.Point(5, 3);
+            this.ptr_User.Name = "ptr_User";
+            this.ptr_User.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.ptr_User.ShadowDecoration.Parent = this.ptr_User;
+            this.ptr_User.Size = new System.Drawing.Size(40, 40);
+            this.ptr_User.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptr_User.TabIndex = 1;
+            this.ptr_User.TabStop = false;
+            // 
+            // btnMenuLateral
+            // 
+            this.btnMenuLateral.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnMenuLateral.BackColor = System.Drawing.Color.Transparent;
+            this.btnMenuLateral.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMenuLateral.Image = ((System.Drawing.Image)(resources.GetObject("btnMenuLateral.Image")));
+            this.btnMenuLateral.ImageActive = null;
+            this.btnMenuLateral.Location = new System.Drawing.Point(6, 15);
+            this.btnMenuLateral.Name = "btnMenuLateral";
+            this.btnMenuLateral.Size = new System.Drawing.Size(25, 25);
+            this.btnMenuLateral.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMenuLateral.TabIndex = 17;
+            this.btnMenuLateral.TabStop = false;
+            this.btnMenuLateral.Zoom = 10;
             // 
             // DashboardPrincipal
             // 
@@ -36,15 +284,50 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(245)))), ((int)(((byte)(252)))));
             this.ClientSize = new System.Drawing.Size(1204, 650);
+            this.Controls.Add(this.panelCabecalho);
+            this.Controls.Add(this.P_I);
+            this.Controls.Add(this.P_S);
+            this.Controls.Add(this.P_L);
+            this.Controls.Add(this.panelLateral);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "DashboardPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Glass Dashboard";
             this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panelCabecalho.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panelUsuario.ResumeLayout(false);
+            this.panelUsuario.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnTrocarUsuario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptr_User)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMenuLateral)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel P_I;
+        private System.Windows.Forms.Panel P_S;
+        private Bunifu.Framework.UI.BunifuGradientPanel panelLateral;
+        private System.Windows.Forms.Panel P_L;
+        private Bunifu.Framework.UI.BunifuCards panelCabecalho;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2ImageButton btnResumoSystem;
+        private Guna.UI2.WinForms.Guna2ImageButton btnHome;
+        private System.Windows.Forms.Label lblTotalNotificacao;
+        private Guna.UI2.WinForms.Guna2ImageButton btnNotificacao;
+        private System.Windows.Forms.Panel panelUsuario;
+        private Bunifu.Framework.UI.BunifuImageButton btnTrocarUsuario;
+        private System.Windows.Forms.Label lblNivelUsuario;
+        private System.Windows.Forms.Label lblNomeUsuario;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox ptr_User;
+        private Bunifu.Framework.UI.BunifuImageButton btnMenuLateral;
+
+
     }
 }
