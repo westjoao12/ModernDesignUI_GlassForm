6ed6363c04a7e69f55978f3d662f0be90eb82b51
westjoao12 <westjoao12@gmail.com>
2024-11-06 03:00:48 +0100

    create project's form

---
+++ ModernDesignUI_GlassForm/Forms Principal/FormProject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModernDesignUI_GlassForm.Forms_Principal
+{
+    public partial class FormProject : Form
+    {
+        public FormProject()
+        {
+            InitializeComponent();
+        }
+    }
+}
